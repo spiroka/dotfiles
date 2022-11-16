3ed63d01b1d7b9d7fa7dc956e3fe19c45557ed8c
Gergő Takács <spiroka01@gmail.com>
2022-11-16 11:19:01 +0100

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,8 @@
 require('plugins')
 local lspconfig = require('lspconfig')
+local telescope = require('telescope')
+local cmp = require('cmp')
+local luasnip = require('luasnip')
 
 -- Setting NVim options
 vim.opt.expandtab = true
@@ -15,8 +18,16 @@ vim.opt.hlsearch = false
 vim.opt.list = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
+vim.opt.completeopt = 'menu,menuone,noselect'
 
 -- LSP setup
+local lsp_defaults = lspconfig.util.default_config
+lsp_defaults.capabilities = vim.tbl_deep_extend(
+  'force',
+  lsp_defaults.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)
+
 lspconfig.sumneko_lua.setup {
   settings = {
     Lua = {
@@ -39,6 +50,10 @@ lspconfig.sumneko_lua.setup {
     },
   },
 }
+lspconfig.tsserver.setup({})
+lspconfig.stylelint_lsp.setup({})
+lspconfig.emmet_ls.setup({})
+lspconfig.eslint.setup({})
 
 -- Ayu theme setup
 require('ayu').setup({
@@ -59,4 +74,47 @@ require('lualine').setup({
     lualine_y = {},
     lualine_z = {'location'}
   }
+})
+
+-- Telescope setup
+telescope.setup()
+telescope.load_extension('fzf')
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-b>', builtin.buffers, {})
+vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
+
+-- nvim-cmp setup
+require('luasnip.loaders.from_vscode').lazy_load()
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end
+  },
+  sources = {
+    {name = 'path'},
+    {name = 'nvim_lsp', keyword_length = 3},
+    {name = 'buffer', keyword_length = 3},
+    {name = 'luasnip', keyword_length = 2},
+  },
+  mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          local entry = cmp.get_selected_entry()
+          if not entry then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          else
+            cmp.confirm()
+          end
+        else
+          fallback()
+        end
+      end, {"i","s","c",})
+  }),
 })
